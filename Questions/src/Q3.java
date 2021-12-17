06a7711e57859469d3b7bb4639da0f222992ffb6
chendong09 <chendong09@baidu.com>
2021-12-17 16:30:00 +0800

    3

---
+++ Questions/src/Q3.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ *
+ * @author chendong09
+ * @since 2021/12/17
+ **/
+public class Q3 {
+
+    /**
+     *
+     * 通过map记录遍历的字符和索引位置
+     * 循环遍历输入字符串，如果遇到已出现的字符，则将起始索引重置为最大的索引位置
+     *
+     * @param s 输入字符串
+     * @return 最长子串长度
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0; int start = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)) {
+                start = Math.max(map.get(ch) + 1, start);
+            }
+            max = Math.max(max, end - start + 1);
+            map.put(ch, end);
+        }
+        return max;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}

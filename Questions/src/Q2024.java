ece9b90ab8274d507fa9eb5b343a2913ab893e93
chendong09 <chendong09@baidu.com>
2022-03-29 17:51:55 +0800

    2024

---
+++ Questions/src/Q2024.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * 给你一个字符串 answerKey ，其中 answerKey[i] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
+ *
+ * 每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey[i] 改为 'T' 或者 'F' ）。
+ * 请你返回在不超过 k 次操作的情况下，最大 连续 'T' 或者 'F' 的数目。
+ *
+ * 输入：answerKey = "TTFF", k = 2
+ * 输出：4
+ * 解释：我们可以将两个 'F' 都变为 'T' ，得到 answerKey = "TTTT" 。
+ * 总共有四个连续的 'T' 。
+ *
+ * @author chendong09
+ * @since 2022/03/29
+ */
+public class Q2024 {
+
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        return Math.max(maxConsecutiveAnswers(answerKey, k, 'T'), maxConsecutiveAnswers(answerKey, k, 'F'));
+    }
+
+    public int maxConsecutiveAnswers(String answerKey, int k, char ch) {
+        int n = answerKey.length();
+        int ans = 0;
+        for (int left = 0, right = 0, sum = 0; right < n; right++) {
+            sum += answerKey.charAt(right) != ch ? 1 : 0;
+            while (sum > k) {
+                sum -= answerKey.charAt(left++) != ch ? 1 : 0;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}

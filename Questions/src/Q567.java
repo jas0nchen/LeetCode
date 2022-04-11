2277eda48d489a2d1f5eace3a85a782e4dab471a
chendong09 <chendong09@baidu.com>
2022-04-11 17:17:12 +0800

    567

---
+++ Questions/src/Q567.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+
+/**
+ * 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+ *
+ * 换句话说，s1 的排列之一是 s2 的 子串 。
+ *
+ * @author chendong09
+ * @since 2022/04/11
+ */
+public class Q567 {
+
+    /**
+     * 输入：s1 = "ab" s2 = "eidbaooo"
+     * 输出：true
+     * 解释：s2 包含 s1 的排列之一 ("ba").
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; ++i) {
+            ++cnt1[s1.charAt(i) - 'a'];
+            ++cnt2[s2.charAt(i) - 'a'];
+        }
+        if (Arrays.equals(cnt1, cnt2)) {
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            ++cnt2[s2.charAt(i) - 'a'];
+            --cnt2[s2.charAt(i - n) - 'a'];
+            if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 优化版，只比较差异
+     *
+     * s1 = "ab" s2 = "eidbaooo"
+     *
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean checkInclusion2(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s1.charAt(i) - 'a'];
+            ++cnt[s2.charAt(i) - 'a'];
+        }
+        int diff = 0;
+        for (int c : cnt) {
+            if (c != 0) {
+                ++diff;
+            }
+        }
+        if (diff == 0) {
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            int x = s2.charAt(i) - 'a', y = s2.charAt(i - n) - 'a';
+            if (x == y) {
+                continue;
+            }
+            if (cnt[x] == 0) {
+                ++diff;
+            }
+            ++cnt[x];
+            if (cnt[x] == 0) {
+                --diff;
+            }
+            if (cnt[y] == 0) {
+                ++diff;
+            }
+            --cnt[y];
+            if (cnt[y] == 0) {
+                --diff;
+            }
+            if (diff == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
